a98d718869049afbda3ff5549bc3616bd1d7be4e
heyzoos <jbracho@uchicago.edu>
2017-06-08 12:25:48 -0500

    Running build on Ubuntu computer

--- build.sh
+++ build.sh
@@ -1,7 +1,34 @@
 #! /bin/bash
 
+pip install virtualenv
+virtualenv env
+
 source env/bin/activate
 pip install -r requirements.txt
 pip install pyinstaller
 pyinstaller --name=nodewatchd manage.py -y
 cp -r env/lib/python3.5/site-packages/rest_framework/templates/ dist/rest_framework_templates
+
+# Build the .deb file
+mkdir -p deb_build/usr/local/bin/
+cp -r dist/ deb_build/usr/local/bin/nodewatchd
+cp nodewatch.service deb_build/
+mkdir deb_build/DEBIAN
+
+echo "Package: nodewatch" >> deb_build/DEBIAN/control
+echo "Maintainer: nodewatch" >> deb_build/DEBIAN/control
+echo "Description: nodewatch" >> deb_build/DEBIAN/control
+echo "Version: 0.0.0" >> deb_build/DEBIAN/control
+echo "Architecture: i386" >> deb_build/DEBIAN/control
+
+echo "mv nodewatch.service /etc/systemd/system/" >> deb_build/DEBIAN/postinst
+echo "systemctl start nodewatch" >> deb_build/DEBIAN/postinst
+
+chmod 775 deb_build/DEBIAN/postinst
+
+mv deb_build/ nodewatchd/
+dpkg-deb --build nodewatchd
+
+# Clean up
+rm nodewatchd.spec
+rm -rf build/ dist/ nodewatchd/
